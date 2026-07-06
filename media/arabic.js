57c8a1f77114178334b7482ebcd19e6b75c4a5bd
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:53:07 +0300

    Add arabicSeries data to arabic.js

---
+++ media/arabic.js
@@ -0,0 +1,7 @@
+export const arabicSeries = [
+  { title: "Bab Al-Hara", year: "2006", episodes: "10 Seasons", rating: "8.5", download: "https://shahid.mbc.net/ar/series/bab-al-hara" },
+  { title: "Al-Taghreba Al-Falastenya", year: "2018", episodes: "1 Season", rating: "8.3", download: "https://shahid.mbc.net/ar/series/al-taghreba-al-falastenya" },
+  { title: "The Writer", year: "2020", episodes: "1 Season", rating: "8.0", download: "https://shahid.mbc.net/ar/series/the-writer" },
+  { title: "Eish Al-Tayer", year: "2019", episodes: "1 Season", rating: "7.8", download: "https://shahid.mbc.net/ar/series/eish-al-tayer" },
+  { title: "Weld Al-Ghalaba", year: "2021", episodes: "1 Season", rating: "7.5", download: "https://shahid.mbc.net/ar/series/weld-al-ghalaba" },
+];
