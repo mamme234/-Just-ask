c2b5701add212659688dfd0c5ade7a02dbe987e0
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:50:42 +0300

    Add kdramas list with details and download links

---
+++ media/kdrama.js
@@ -0,0 +1,47 @@
+export const kdramas = [
+  // ===== TOP K-DRAMAS =====
+  { title: "Crash Landing on You", year: "2019", episodes: "16", rating: "9.0", download: "https://kissasian.li/drama/crash-landing-on-you" },
+  { title: "Descendants of the Sun", year: "2016", episodes: "16", rating: "8.8", download: "https://kissasian.li/drama/descendants-of-the-sun" },
+  { title: "Goblin", year: "2016", episodes: "16", rating: "8.9", download: "https://kissasian.li/drama/goblin" },
+  { title: "Itaewon Class", year: "2020", episodes: "16", rating: "8.6", download: "https://kissasian.li/drama/itaewon-class" },
+  { title: "Kingdom", year: "2019", episodes: "12", rating: "8.9", download: "https://kissasian.li/drama/kingdom" },
+  { title: "Squid Game", year: "2021", episodes: "9", rating: "8.8", download: "https://kissasian.li/drama/squid-game" },
+  { title: "Vincenzo", year: "2021", episodes: "20", rating: "8.7", download: "https://kissasian.li/drama/vincenzo" },
+  { title: "Hospital Playlist", year: "2020", episodes: "12", rating: "8.8", download: "https://kissasian.li/drama/hospital-playlist" },
+  { title: "Reply 1988", year: "2015", episodes: "20", rating: "9.0", download: "https://kissasian.li/drama/reply-1988" },
+  { title: "Mr. Sunshine", year: "2018", episodes: "24", rating: "8.9", download: "https://kissasian.li/drama/mr-sunshine" },
+  { title: "My Mister", year: "2018", episodes: "16", rating: "9.0", download: "https://kissasian.li/drama/my-mister" },
+  { title: "Signal", year: "2016", episodes: "16", rating: "9.0", download: "https://kissasian.li/drama/signal" },
+  
+  // ===== ROMANTIC K-DRAMAS =====
+  { title: "What's Wrong with Secretary Kim", year: "2018", episodes: "16", rating: "8.1", download: "https://kissasian.li/drama/whats-wrong-with-secretary-kim" },
+  { title: "My Love from the Star", year: "2013", episodes: "21", rating: "8.4", download: "https://kissasian.li/drama/my-love-from-the-star" },
+  { title: "The King: Eternal Monarch", year: "2020", episodes: "16", rating: "8.1", download: "https://kissasian.li/drama/the-king-eternal-monarch" },
+  { title: "Start-Up", year: "2020", episodes: "16", rating: "8.0", download: "https://kissasian.li/drama/start-up" },
+  { title: "It's Okay to Not Be Okay", year: "2020", episodes: "16", rating: "8.6", download: "https://kissasian.li/drama/its-okay-to-not-be-okay" },
+  { title: "Hotel del Luna", year: "2019", episodes: "16", rating: "8.7", download: "https://kissasian.li/drama/hotel-del-luna" },
+  { title: "Business Proposal", year: "2022", episodes: "12", rating: "8.2", download: "https://kissasian.li/drama/business-proposal" },
+  { title: "Twenty-Five Twenty-One", year: "2022", episodes: "16", rating: "8.5", download: "https://kissasian.li/drama/twenty-five-twenty-one" },
+  { title: "Our Blues", year: "2022", episodes: "20", rating: "8.5", download: "https://kissasian.li/drama/our-blues" },
+  { title: "Strong Woman Do Bong-soon", year: "2017", episodes: "16", rating: "8.1", download: "https://kissasian.li/drama/strong-woman-do-bong-soon" },
+  { title: "Weightlifting Fairy Kim Bok-joo", year: "2016", episodes: "16", rating: "8.4", download: "https://kissasian.li/drama/weightlifting-fairy-kim-bok-joo" },
+  { title: "While You Were Sleeping", year: "2017", episodes: "32", rating: "8.4", download: "https://kissasian.li/drama/while-you-were-sleeping" },
+  { title: "The Legend of the Blue Sea", year: "2016", episodes: "20", rating: "8.1", download: "https://kissasian.li/drama/the-legend-of-the-blue-sea" },
+  { title: "W: Two Worlds", year: "2016", episodes: "16", rating: "8.3", download: "https://kissasian.li/drama/w-two-worlds" },
+  { title: "Fight for My Way", year: "2017", episodes: "16", rating: "8.0", download: "https://kissasian.li/drama/fight-for-my-way" },
+  { title: "She Was Pretty", year: "2015", episodes: "16", rating: "7.9", download: "https://kissasian.li/drama/she-was-pretty" },
+  
+  // ===== CLASSIC K-DRAMAS =====
+  { title: "Coffee Prince", year: "2007", episodes: "17", rating: "8.3", download: "https://kissasian.li/drama/coffee-prince" },
+  { title: "Secret Garden", year: "2010", episodes: "20", rating: "8.2", download: "https://kissasian.li/drama/secret-garden" },
+  { title: "Boys Over Flowers", year: "2009", episodes: "25", rating: "7.9", download: "https://kissasian.li/drama/boys-over-flowers" },
+  { title: "Full House", year: "2004", episodes: "16", rating: "7.8", download: "https://kissasian.li/drama/full-house" },
+  { title: "Winter Sonata", year: "2002", episodes: "20", rating: "8.0", download: "https://kissasian.li/drama/winter-sonata" },
+  { title: "Stairway to Heaven", year: "2003", episodes: "20", rating: "8.1", download: "https://kissasian.li/drama/stairway-to-heaven" },
+  { title: "Autumn in My Heart", year: "2000", episodes: "16", rating: "8.0", download: "https://kissasian.li/drama/autumn-in-my-heart" },
+  
+  // ===== NEW K-DRAMAS =====
+  { title: "Extraordinary Attorney Woo", year: "2022", episodes: "16", rating: "8.9", download: "https://kissasian.li/drama/extraordinary-attorney-woo" },
+  { title: "Alchemy of Souls", year: "2022", episodes: "30", rating: "8.8", download: "https://kissasian.li/drama/alchemy-of-souls" },
+  { title: "The Glory", year: "2022", episodes: "16", rating: "8.8", download: "https://kissasian.li/drama/the-glory" },
+];
