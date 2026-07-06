055f90859999f4434f5f46d1e403d5b83d2caa1d
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:54:23 +0300

    Add japaneseMovies list with details of films

---
+++ media/japanese.js
@@ -0,0 +1,14 @@
+export const japaneseMovies = [
+  { title: "Seven Samurai", year: "1954", genre: "Action", rating: "8.6", download: "https://yts.mx/movies/seven-samurai-1954" },
+  { title: "Rashomon", year: "1950", genre: "Drama", rating: "8.2", download: "https://yts.mx/movies/rashomon-1950" },
+  { title: "Spirited Away", year: "2001", genre: "Animation", rating: "8.6", download: "https://yts.mx/movies/spirited-away-2001" },
+  { title: "Howl's Moving Castle", year: "2004", genre: "Animation", rating: "8.2", download: "https://yts.mx/movies/howls-moving-castle-2004" },
+  { title: "My Neighbor Totoro", year: "1988", genre: "Animation", rating: "8.2", download: "https://yts.mx/movies/my-neighbor-totoro-1988" },
+  { title: "Princess Mononoke", year: "1997", genre: "Animation", rating: "8.4", download: "https://yts.mx/movies/princess-mononoke-1997" },
+  { title: "Akira", year: "1988", genre: "Animation", rating: "8.0", download: "https://yts.mx/movies/akira-1988" },
+  { title: "Ghost in the Shell", year: "1995", genre: "Sci-Fi", rating: "7.9", download: "https://yts.mx/movies/ghost-in-the-shell-1995" },
+  { title: "Your Name", year: "2016", genre: "Romance", rating: "8.4", download: "https://yts.mx/movies/your-name-2016" },
+  { title: "Weathering With You", year: "2019", genre: "Romance", rating: "7.7", download: "https://yts.mx/movies/weathering-with-you-2019" },
+  { title: "The Garden of Words", year: "2013", genre: "Romance", rating: "7.5", download: "https://yts.mx/movies/the-garden-of-words-2013" },
+  { title: "5 Centimeters Per Second", year: "2007", genre: "Romance", rating: "7.8", download: "https://yts.mx/movies/5-centimeters-per-second-2007" },
+];
