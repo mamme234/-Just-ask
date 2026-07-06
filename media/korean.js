de04b81d1789c6fa99b9fb2a18a4056b9f32c997
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:53:44 +0300

    Add koreanMovies list with details and download links

---
+++ media/korean.js
@@ -0,0 +1,12 @@
+export const koreanMovies = [
+  { title: "Parasite", year: "2019", genre: "Thriller", rating: "8.5", download: "https://yts.mx/movies/parasite-2019" },
+  { title: "Train to Busan", year: "2016", genre: "Horror", rating: "7.6", download: "https://yts.mx/movies/train-to-busan-2016" },
+  { title: "Oldboy", year: "2003", genre: "Thriller", rating: "8.4", download: "https://yts.mx/movies/oldboy-2003" },
+  { title: "The Handmaiden", year: "2016", genre: "Thriller", rating: "8.1", download: "https://yts.mx/movies/the-handmaiden-2016" },
+  { title: "Memories of Murder", year: "2003", genre: "Crime", rating: "8.1", download: "https://yts.mx/movies/memories-of-murder-2003" },
+  { title: "Burning", year: "2018", genre: "Mystery", rating: "7.5", download: "https://yts.mx/movies/burning-2018" },
+  { title: "The Chaser", year: "2008", genre: "Thriller", rating: "7.8", download: "https://yts.mx/movies/the-chaser-2008" },
+  { title: "The Wailing", year: "2016", genre: "Horror", rating: "7.5", download: "https://yts.mx/movies/the-wailing-2016" },
+  { title: "A Taxi Driver", year: "2017", genre: "Drama", rating: "7.9", download: "https://yts.mx/movies/a-taxi-driver-2017" },
+  { title: "Spring, Summer, Fall, Winter... and Spring", year: "2003", genre: "Drama", rating: "8.0", download: "https://yts.mx/movies/spring-summer-fall-winter-and-spring-2003" },
+];
