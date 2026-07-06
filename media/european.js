9edea89d7fe0961cd3e26d25a4d743d471de47e7
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:54:57 +0300

    Add list of European movies with details

---
+++ media/european.js
@@ -0,0 +1,35 @@
+export const europeanMovies = [
+  // ===== FRENCH =====
+  { title: "Amélie", year: "2001", genre: "Romance", rating: "8.3", download: "https://yts.mx/movies/amelie-2001" },
+  { title: "The Intouchables", year: "2011", genre: "Comedy", rating: "8.5", download: "https://yts.mx/movies/the-intouchables-2011" },
+  { title: "Léon: The Professional", year: "1994", genre: "Action", rating: "8.5", download: "https://yts.mx/movies/leon-the-professional-1994" },
+  { title: "The Fifth Element", year: "1997", genre: "Sci-Fi", rating: "7.6", download: "https://yts.mx/movies/the-fifth-element-1997" },
+  { title: "La Haine", year: "1995", genre: "Drama", rating: "8.1", download: "https://yts.mx/movies/la-haine-1995" },
+  
+  // ===== ITALIAN =====
+  { title: "Cinema Paradiso", year: "1988", genre: "Drama", rating: "8.5", download: "https://yts.mx/movies/cinema-paradiso-1988" },
+  { title: "Life is Beautiful", year: "1997", genre: "Comedy", rating: "8.6", download: "https://yts.mx/movies/life-is-beautiful-1997" },
+  { title: "The Great Beauty", year: "2013", genre: "Drama", rating: "7.7", download: "https://yts.mx/movies/the-great-beauty-2013" },
+  
+  // ===== SPANISH =====
+  { title: "Pan's Labyrinth", year: "2006", genre: "Fantasy", rating: "8.2", download: "https://yts.mx/movies/pans-labyrinth-2006" },
+  { title: "The Sea Inside", year: "2004", genre: "Drama", rating: "8.0", download: "https://yts.mx/movies/the-sea-inside-2004" },
+  { title: "Volver", year: "2006", genre: "Comedy", rating: "7.6", download: "https://yts.mx/movies/volver-2006" },
+  
+  // ===== GERMAN =====
+  { title: "The Lives of Others", year: "2006", genre: "Drama", rating: "8.4", download: "https://yts.mx/movies/the-lives-of-others-2006" },
+  { title: "Good Bye Lenin!", year: "2003", genre: "Comedy", rating: "7.7", download: "https://yts.mx/movies/good-bye-lenin-2003" },
+  { title: "Run Lola Run", year: "1998", genre: "Action", rating: "7.7", download: "https://yts.mx/movies/run-lola-run-1998" },
+  
+  // ===== BRITISH =====
+  { title: "The King's Speech", year: "2010", genre: "Drama", rating: "8.0", download: "https://yts.mx/movies/the-kings-speech-2010" },
+  { title: "Slumdog Millionaire", year: "2008", genre: "Drama", rating: "8.0", download: "https://yts.mx/movies/slumdog-millionaire-2008" },
+  { title: "Trainspotting", year: "1996", genre: "Drama", rating: "8.1", download: "https://yts.mx/movies/trainspotting-1996" },
+  { title: "Lock, Stock and Two Smoking Barrels", year: "1998", genre: "Comedy", rating: "8.1", download: "https://yts.mx/movies/lock-stock-and-two-smoking-barrels-1998" },
+  { title: "Snatch", year: "2000", genre: "Comedy", rating: "8.3", download: "https://yts.mx/movies/snatch-2000" },
+  
+  // ===== RUSSIAN =====
+  { title: "The Cranes Are Flying", year: "1957", genre: "Drama", rating: "8.0", download: "https://yts.mx/movies/the-cranes-are-flying-1957" },
+  { title: "Solaris", year: "1972", genre: "Sci-Fi", rating: "8.0", download: "https://yts.mx/movies/solaris-1972" },
+  { title: "Stalker", year: "1979", genre: "Sci-Fi", rating: "8.0", download: "https://yts.mx/movies/stalker-1979" },
+];
