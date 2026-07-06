8569c6574038442424fcfd5b8f1afb0cd7e2a232
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-06 15:49:41 +0300

    Add Turkish series data to turkish.js

---
+++ media/turkish.js
@@ -0,0 +1,46 @@
+export const turkishSeries = [
+  // ===== HISTORICAL TURKISH SERIES =====
+  { title: "Diriliş: Ertuğrul", year: "2014", episodes: "5 Seasons", rating: "8.9", download: "https://turkish123.com/drama/dirilis-ertugrul" },
+  { title: "Kuruluş: Osman", year: "2019", episodes: "5 Seasons", rating: "8.7", download: "https://turkish123.com/drama/kurulus-osman" },
+  { title: "Muhteşem Yüzyıl", year: "2011", episodes: "4 Seasons", rating: "8.8", download: "https://turkish123.com/drama/muhtesem-yuzyil" },
+  { title: "Muhteşem Yüzyıl: Kösem", year: "2015", episodes: "2 Seasons", rating: "8.0", download: "https://turkish123.com/drama/muhtesem-yuzyil-kosem" },
+  { title: "Payitaht Abdülhamid", year: "2017", episodes: "5 Seasons", rating: "8.3", download: "https://turkish123.com/drama/payitaht-abdulhamid" },
+  { title: "Mehmed: Fetihler Sultanı", year: "2024", episodes: "1 Season", rating: "8.1", download: "https://turkish123.com/drama/mehmed-fetihler-sultani" },
+  { title: "Kudüs Fatihi Selahaddin Eyyubi", year: "2023", episodes: "2 Seasons", rating: "8.2", download: "https://turkish123.com/drama/selahaddin-eyyubi" },
+  
+  // ===== ROMANTIC TURKISH SERIES =====
+  { title: "Kara Sevda", year: "2015", episodes: "2 Seasons", rating: "8.5", download: "https://turkish123.com/drama/kara-sevda" },
+  { title: "Aşk-ı Memnu", year: "2008", episodes: "2 Seasons", rating: "8.4", download: "https://turkish123.com/drama/ask-i-memnu" },
+  { title: "Fatmagül'ün Suçu Ne?", year: "2010", episodes: "2 Seasons", rating: "8.6", download: "https://turkish123.com/drama/fatmagulun-sucu-ne" },
+  { title: "Kuzey Güney", year: "2011", episodes: "2 Seasons", rating: "8.3", download: "https://turkish123.com/drama/kuzey-guney" },
+  { title: "Hercai", year: "2019", episodes: "3 Seasons", rating: "8.4", download: "https://turkish123.com/drama/hercai" },
+  { title: "Erkenci Kuş", year: "2018", episodes: "1 Season", rating: "8.3", download: "https://turkish123.com/drama/erkenci-kus" },
+  { title: "Sen Çal Kapımı", year: "2020", episodes: "1 Season", rating: "8.1", download: "https://turkish123.com/drama/sen-cal-kapimi" },
+  { title: "Kiralık Aşk", year: "2015", episodes: "2 Seasons", rating: "8.2", download: "https://turkish123.com/drama/kiralik-ask" },
+  { title: "Aşk Laftan Anlamaz", year: "2016", episodes: "1 Season", rating: "8.0", download: "https://turkish123.com/drama/ask-laflen-anlamaz" },
+  { title: "Bay Yanlış", year: "2020", episodes: "1 Season", rating: "7.8", download: "https://turkish123.com/drama/bay-yanlis" },
+  { title: "Aşk 101", year: "2020", episodes: "2 Seasons", rating: "8.0", download: "https://turkish123.com/drama/ask-101" },
+  { title: "Aşk ve Ceza", year: "2010", episodes: "2 Seasons", rating: "7.8", download: "https://turkish123.com/drama/ask-ve-ceza" },
+  { title: "Elif", year: "2014", episodes: "6 Seasons", rating: "7.8", download: "https://turkish123.com/drama/elif" },
+  
+  // ===== DRAMA TURKISH SERIES =====
+  { title: "Ezel", year: "2009", episodes: "2 Seasons", rating: "8.7", download: "https://turkish123.com/drama/ezel" },
+  { title: "Içerde", year: "2016", episodes: "1 Season", rating: "8.3", download: "https://turkish123.com/drama/icerde" },
+  { title: "Savaşçı", year: "2017", episodes: "3 Seasons", rating: "8.0", download: "https://turkish123.com/drama/savasci" },
+  { title: "Kara Para Aşk", year: "2014", episodes: "2 Seasons", rating: "8.2", download: "https://turkish123.com/drama/kara-para-ask" },
+  { title: "Cesur ve Güzel", year: "2016", episodes: "1 Season", rating: "7.9", download: "https://turkish123.com/drama/cesur-ve-guzel" },
+  { title: "Medcezir", year: "2013", episodes: "2 Seasons", rating: "8.1", download: "https://turkish123.com/drama/medcezir" },
+  { title: "Paramparça", year: "2014", episodes: "3 Seasons", rating: "8.0", download: "https://turkish123.com/drama/paramparca" },
+  { title: "Sadakatsiz", year: "2020", episodes: "2 Seasons", rating: "7.8", download: "https://turkish123.com/drama/sadakatsiz" },
+  { title: "Yasak Elma", year: "2018", episodes: "7 Seasons", rating: "7.5", download: "https://turkish123.com/drama/yasak-elma" },
+  { title: "Sen Anlat Karadeniz", year: "2018", episodes: "3 Seasons", rating: "8.1", download: "https://turkish123.com/drama/sen-anlat-karadeniz" },
+  { title: "Mucize Doktor", year: "2019", episodes: "3 Seasons", rating: "8.0", download: "https://turkish123.com/drama/mucize-doktor" },
+  
+  // ===== COMEDY TURKISH SERIES =====
+  { title: "Avrupa Yakası", year: "2004", episodes: "10 Seasons", rating: "8.5", download: "https://turkish123.com/drama/avrupa-yakasi" },
+  { title: "Kardeş Payı", year: "2014", episodes: "2 Seasons", rating: "8.2", download: "https://turkish123.com/drama/kardes-payi" },
+  { title: "Güldür Güldür Show", year: "2013", episodes: "8 Seasons", rating: "8.0", download: "https://turkish123.com/drama/guldur-guldur-show" },
+  { title: "Jet Sosyete", year: "2018", episodes: "2 Seasons", rating: "7.8", download: "https://turkish123.com/drama/jet-sosyete" },
+  { title: "Aile", year: "2023", episodes: "1 Season", rating: "8.0", download: "https://turkish123.com/drama/aile" },
+  { title: "Yaprak Dökümü", year: "2006", episodes: "5 Seasons", rating: "8.1", download: "https://turkish123.com/drama/yaprak-dokumu" },
+];
